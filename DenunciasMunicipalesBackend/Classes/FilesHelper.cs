443dff8783b4086bd0d70277258e97515f413dd6
NestorEduardo <nestoredelacruz@gmail.com>
2017-04-24 10:11:52 -0400

    Last deploy

--- DenunciasMunicipalesBackend/Classes/FilesHelper.cs
+++ DenunciasMunicipalesBackend/Classes/FilesHelper.cs
@@ -15,11 +15,6 @@ namespace DenunciasMunicipalesBackend.Classes
                 pic = Path.GetFileName(file.FileName);
                 path = Path.Combine(HttpContext.Current.Server.MapPath(folder), pic);
                 file.SaveAs(path);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    file.InputStream.CopyTo(ms);
-                    byte[] array = ms.GetBuffer();
-                }
             }
 
             return pic;
--- DenunciasMunicipalesBackend/Models/ComplaintView.cs
+++ DenunciasMunicipalesBackend/Models/ComplaintView.cs
@@ -1,28 +1,12 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
 namespace DenunciasMunicipalesBackend.Models
 {
-    public class ComplaintView
+    [NotMapped]
+    public class ComplaintView : Complaint
     {
-        public int ComplaintId { get; set; }
-
-        [Display(Name = "Descripción")]
-        [StringLength(200, ErrorMessage = "El campo {0} debe estar entre {2} y {1} caracteres", MinimumLength = 3)]
-        public string Description { get; set; }
-
-        [Display(Name = "Dirección del caso")]
-        public string CaseAddress { get; set; }
-
-        [Display(Name = "Fecha")]
-        public DateTime Date { get; set; }
-
-        [Display(Name = "Creado por")]
-        public string CreatedBy { get; set; }
-
-        public string Image { get; set; }
-
         [Display(Name = "Imágen")]
         public HttpPostedFileBase ImageFile { get; set; }
     }
